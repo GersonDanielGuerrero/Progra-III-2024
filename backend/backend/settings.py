0b3a08a19d34edb3385106fede953276959026d4
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-09-23 07:34:32 -0600

    instalacion de uso de JWT

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,11 +77,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'good_burger',
+        'USER': 'usuario_good_burger',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
