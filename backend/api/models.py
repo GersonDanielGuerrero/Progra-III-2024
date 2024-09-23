43d0789fa00b21720ae263b3f57305638916b747
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-09-22 21:08:15 -0600

    Modelo usuario añadido

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
+
+class Usuario(AbstractBaseUser):
+    correo = models.EmailField(max_length=200, unique=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=9)
+    contraseña = models.CharField(max_length=128)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    
+    USERNAME_FIELD = 'correo'
+    REQUIRED_FIELDS = ['nombre', 'apellido', 'telefono','contraseña']
+    
