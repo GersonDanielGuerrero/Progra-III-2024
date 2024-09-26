b8238cc266a4580432da2b81a1ca91f00bf4526d
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-09-25 21:05:16 -0600

    Primeras vistas añadidas y rutas configuradas

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import Registro from "../views/RegistroUsuario.vue";
+import Login from "../views/LogIn.vue";
+import Principal from "../views/PaginaPrincipal.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "Principal",
+        component: Principal
+    },
+    {
+        path: "/registro",
+        name: "Registro",
+        component: Registro,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+    },
+];
+
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
+    routes,
 });
 
 export default router;
-    
---
+++ frontend/src/views/LogIn.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<style scoped>
+    
+
+</style>
+
+<!-- C U I D A D I T O -->
+
+<script>
+export default {
+    name: 'LogIn',
+    data() {
+        return {
+            
+        }
+    },
+    props: {
+        
+    },
+    methods: {
+        
+    },
+    computed: {
+        
+    },
+
+}
+</script>
