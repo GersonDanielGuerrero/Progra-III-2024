cbc13b80f0fe2f90870798b55a0ab481900a68d5
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-09-28 10:37:20 -0600

    Registro completado

--- backend/usuarios/api.py
+++
@@ -1,8 +0,0 @@
-from usuarios.models import Usuario, Direccion, Rol
-from rest_framework import viewsets, permissions
-from .serializers import RegistroSerializer
-
-class RegistroViewSet(viewsets.ModelViewSet):
-    def create(self, request):
-        serializer = RegistroSerializer(data=request.data)
-        
