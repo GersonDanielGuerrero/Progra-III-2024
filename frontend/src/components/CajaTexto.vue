6cf7369b03efec674dd4ab5ce7d295895b6ec432
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-09-29 14:58:01 -0600

    Correcciones al icono y atributos

--- frontend/src/components/CajaTexto.vue
+++ frontend/src/components/CajaTexto.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="input-group">
-    <input :type="type" :placeholder="placeholder" />
-    <i class="icon" v-if="icon">{{ icon }}</i>
+    <input 
+        v-bind="$attrs"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <img class="icono" :src="icono" v-if="icono">
     </div>
 </template>
 
 <style scoped>
-    .input-group {
+*{
+    font-family: "Arial Black", Gadget, sans-serif;
+}
+.input-group {
 position: relative;
 margin-bottom: 15px;
 width: 100%;
@@ -17,8 +24,8 @@ width: 100%;
 padding: 10px;
 padding-right: 40px;
 background-color: rgba(255, 255, 255, 0.3);
-border: none;
 border-radius: 5px;
+border: none;
 color: #000;
 }
 
@@ -26,7 +33,9 @@ color: #000;
 color: #ffad00;
 }
 
-.input-group i.icon {
+.icono {
+height: 30px;
+width: 30px;
 position: absolute;
 right: 10px;
 top: 50%;
@@ -43,11 +52,17 @@ export default {
     name: 'CajaTexto',
     data() {
         return {
-            
         }
     },
     props: {
-        
+        modelValue: {
+            type: String,
+            default: ''
+        },
+        icono: {
+            type: String,
+            Required: false
+        }
     },
     methods: {
         
