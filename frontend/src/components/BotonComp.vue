55c9098fbe510ea298db85ea61318008c34e4d7a
FREDYS DANIEL AMAYA VARGAS <USSS031223@ugb.edu.sv>
2024-09-28 18:34:31 -0600

    estilos y primeros campos añadidos

--- frontend/src/components/BotonComp.vue
+++ frontend/src/components/BotonComp.vue
@@ -1,11 +1,35 @@
 <template>
     <div>
-        
+        <button 
+        :id="btnRegister" 
+        class="btn-register"
+        @click="metodoClick">Registrar
+    <i class="icon" v-if="icon">{{ icon }}</i></button>
     </div>
 </template>
 
 <style scoped>
-    
+    .btn-register {
+        background-color: #ffad00;
+        color: #000;
+        border: none;
+        padding: 10px;
+        cursor: pointer;
+        width: 200px;
+        margin-top: 20px;
+        border-radius: 5px;
+    }
+
+    .btn-register i.icon {
+        margin-right: 10px;
+        color: #fff;
+    }
+
+    .btn-register:hover {
+        opacity: 0.9;
+    }
+
+
 
 </style>
 
