f79c5beb542258d109a0ad90ce7519b2e6cfdda6
FREDYS DANIEL AMAYA VARGAS <USSS031223@ugb.edu.sv>
2024-09-29 19:14:40 -0600

    primeros campos añadidos del diseño registro

--- frontend/src/views/RegistroUsuario.vue
+++ frontend/src/views/RegistroUsuario.vue
@@ -1,18 +1,64 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <div class="logo">
+            <img src="../../public/imagenes/logo_color.png" alt="Good Burguer Logo" />
+        </div>
+        <form class="register-form">
+            <CajaTexto 
+            v-bind:placeholder="'Nombres'"
+            v-bind:icon-src="''">
+            </CajaTexto>
 
+            <CajaTexto
+            v-bind:placeholder="'Apellidos'"
+            v-bind:icon-src="''">
+                
+            </CajaTexto>
+
+            <CajaTexto
+            v-bind:placeholder="'Correo Electronico'"
+            v-bind:icon-src="''"
+            v-bind:is-half="true">
+            </CajaTexto>
+
+            <CajaTexto
+            v-bind:placeholder="'Contraseña'"
+            v-bind:icon-src="''">
+            </CajaTexto>
+
+            <CajaTexto 
+            v-bind:placeholder="'Confirmar Contraseña'"
+            v-bind:icon-src="''">
+            </CajaTexto>
+
+            <BotonComp v-bind:label="'Registrar'" v-bind:icon-src="''"></BotonComp>
+        </form>
     </div>
 </template>
 
 <style scoped>
-    
+.container{
+    text-align: center;
+}
 
+.logo img{
+    width: 150px;
+    margin-bottom: 20px;
+}
+
+.register-form{
+    width: 400px;
+    background-color: #000;
+    padding: 20px;
+    border-radius: 8px;
+}
 </style>
 
 <!-- C U I D A D I T O -->
 
 <script>
+import BotonComp from '@/components/BotonComp.vue';
+import CajaTexto from '@/components/CajaTexto.vue';
 export default {
     name: 'RegistroUsuario',
     data() {
