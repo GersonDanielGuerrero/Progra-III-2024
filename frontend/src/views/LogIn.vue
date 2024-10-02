b754c192aa47005119f43bd6a5a6e9a486ba86a7
Mayre <Mayrenapine30@outlook.es>
2024-10-02 15:07:15 -0600

    Login

--- frontend/src/views/LogIn.vue
+++ frontend/src/views/LogIn.vue
@@ -1,36 +1,35 @@
 <template>
     <div class="login-container">
-      <nav>
-        <a href="#">
+
           <img src="imagenes/imagen de good burger.jpg" alt="imagenes" width="200px">
-        </a>
-      </nav>
+
       <div class="form-wrapper"> 
         <h2>Iniciar sesión</h2>
         <form @submit.prevent="handleSubmit">
           <CajaTexto
-            v-model="text"
+            class="caja-texto"
+            v-model="correo"
             placeholder="Correo Electrónico"
-            type="text"
+            type="email"
             required
           />
           <CajaTexto
+            class="caja-texto"
             v-model="password"
             placeholder="Contraseña"
             type="password"
             required
           />
-  
-          <div class="form-control">
+
             <span>¿Olvidaste tu contraseña?</span>
-          </div>
+
           
-          <BotonComp @click="handleLogin">Iniciar sesión</BotonComp>
+          <BotonComp @metodo_click="iniciar_sesion">Iniciar sesión</BotonComp>
           
           <div class="form-divider">
             <span>¿No tienes una cuenta?</span>
           </div>
-          <BotonComp @click="handleRegister">Registrarse</BotonComp>
+          <BotonComp @metodo_click="registrarse">Registrarse</BotonComp>
 
         </form>
       </div>
@@ -38,6 +37,7 @@
   </template>
 
 <style scoped>
+
     *{
     margin: 0;
     padding: 0;
@@ -51,45 +51,22 @@
     
 }
 
-body{
-  background-color: #000000;
-  margin: 0;
-  padding: 0;
-  height: 100vh;
-  width: 100vw;
-}
-
-body::before{
-    content: "";
-    position: absolute;
-    left: 0;
-    top: 0;
-    opacity: 0.5;
-    width: 100%;
-    height: 100%;
-}
 
 .container{
     text-align: center;
 
 }
 
-nav{
+form{
     display: flex;
     justify-content: center;
-    text-align: center;
     align-items: center;
-    padding: 30px 20px ;
+    flex-direction: column;
 }
 
-nav img{
-    height: 90px;
-    width: 200px;
-    margin-left: 10px;
-    display: block;
-}
 
-.fora-wrapper{
+.form-wrapper{
+    margin: auto;
     position: absolute;
     left: 50%;
     top: 56%;
@@ -97,104 +74,29 @@ nav img{
     padding: 20px;
     width: 600px;
     transform: translate(-50%,-50%);
-    background: rgb(0, 0, 0)
-
+    background: rgb(0, 0, 0);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 }
 
-.fora-wrapper h2{
+.form-wrapper h2{
     color: #fff;
     margin-bottom: 20px;
     text-align: center;
 }
 
-.fora-wrapper form  {
+.form-wrapper form  {
     margin: 25px 0 65px;
 }
 
-form .form-control{
-    height: 50px;
-    position: relative;
-    margin-bottom: 16px;
 
-}
-
- .form-control input{
-   height: 100%;
-   width: 100%;
-   left: 200%;
-    background: hsla(0, 0%, 36%, 0.49);
-    border: none;
-    outline: none;
-    border-radius: 4px;
-    color: #fbfbfb;
-    font-size: 1rem;
-    padding: 0 20px;
-
-}
-
-.form-control input:is(:focus, :valid){
-    background: hsla(0, 6%, 80%, 0.164);
-    padding: 16px 20px 0;
-
-}
-
-.form-control label{
-    position: absolute;
-    left: 20px;
-    top: 50%;
-    transform: translateY(-50%);
-    color: #ffb005;
-    font-size: 1rem;
-    pointer-events: none;
-    transition: all 0.1 ease;
-}
-
-.fora-wrapper input:is(:focus, :valid) ~ label{
-    font-size: 0.75rem;
-    color: rgb(255, 255, 255);
-   transform: translateY(-130%);
-
-
-}
-
-form button{
-    width: 60%; 
-    display: block;
-    margin: auto;
-    padding: 9px 0;
-    font-size: 1rem;
-    background-color : #ffb005;
-    color: #000000;
-    border-radius: 4px;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    transition: .1s ease;
-
-}
-
-form button:hover{
-    background-color: #4444;
-    color: #fffefe;
-}
-
-.fora-wrapper a{
-    text-decoration: none;
-}
-
-.fora-wrapper a:hover{
-    text-decoration: underline;
-}
-
-.fora-wrapper :where(label, p, small, a){
+.form-wrapper :where(label, p, small, a){
     font-size: 0.92rem;
     color: #fff;
 }
 
-form .form-help{
-    display: flex;
-    justify-content: space-between;
-}
 
  .form-divider{
   display: block;
@@ -261,6 +163,10 @@ form .form-help{
     margin-right: -319px;
     font-size: 0.92rem;
 }
+.caja-texto{
+    width: 40vw;
+    max-width: 600px;
+}
 </style>
 
 <!-- C U I D A D I T O -->
@@ -276,7 +182,7 @@ export default {
     },
     data() {
         return {
-            
+
         }
     },
     props: {
