1a108e27bf7995d402747206aca4c765677d0714
Mayre <Mayrenapine30@outlook.es>
2024-10-06 17:24:39 -0600

    ejemplo de categoria

--- frontend/src/components/CategoriaComp.vue
+++ frontend/src/components/CategoriaComp.vue
@@ -1,11 +1,59 @@
 <template>
-    <div>
-        
+   <div class="menu-categories">
+    <div class="category" v-for="category in categories" :key="category.name">
+      <img :src="category.image" alt="category.name" class="category-image" />
+      <h3>{{ category.name }}</h3>
     </div>
+  </div>
 </template>
 
 <style scoped>
+<script>
+export default {
+  data() {
+    return {
+      categories: [
+        {
+          name: 'Burgers',
+          image: 'ruta/a/tu/imagen/burgers.png' 
+        },
+        {
+          name: 'Snacks',
+          image: 'ruta/a/tu/imagen/snacks.png' 
+        },
+        {
+          name: 'Bebidas',
+          image: 'ruta/a/tu/imagen/bebidas.png' 
+        },
+        {
+          name: 'Combos',
+          image: 'ruta/a/tu/imagen/combos.png' 
+        },
+        {
+          name: 'Promociones',
+          image: 'ruta/a/tu/imagen/promociones.png' 
+        }
+      ]
+    };
+  }
+};
+</script>
 
+<style>
+.menu-categories {
+  display: flex;
+  justify-content: space-around;
+  margin: 20px;
+}
+
+.category {
+  text-align: center;
+}
+
+.category-image {
+  width: 100px; 
+  height: auto;
+}
 </style>
 
 
