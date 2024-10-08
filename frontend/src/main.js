96d3f2374f583fa26bfcaf9a38c2f498ce4546d5
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-10-07 18:09:52 -0600

    Pinia incorporado y store auth creado

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,17 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
+import { createPinia } from 'pinia' // Corrected import statement
+import { useAuthStore } from './stores/auth'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
+app.use(router);
+const authStore = useAuthStore();
+authStore.cargarDatos();
+app.mount('#app');
+
+
+
+
