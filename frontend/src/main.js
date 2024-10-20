b4af5f15a341b20882bbb4f440ab81bf3981dd9a
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-10-20 11:04:09 -0600

    Uso de vue 3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import router from './router'
-import { createPinia } from 'pinia' // Corrected import statement
+import { createApp } from 'vue';
+import BootstrapVue3 from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
 const pinia = createPinia();
-app.use(pinia); 
-
+app.use(pinia);
+app.use(BootstrapVue3);
 app.use(router).mount('#app');
 
 
