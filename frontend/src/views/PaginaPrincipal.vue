a1016686dee7b74066042fc4a0a2c8a9d1ee164e
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-10-20 12:51:50 -0600

    modal visible

--- frontend/src/views/PaginaPrincipal.vue
+++ frontend/src/views/PaginaPrincipal.vue
@@ -11,8 +11,34 @@
             >
                 <CategoriaComp class="categoria" :categoria="categoria"/>
             </div>
+            <BotonComp @click="modalCategoria.visible = true">Agregar categoría</BotonComp>
         </div>
+        
     </div>
+    <b-modal class="modal-categoria" v-model="modalCategoria.visible" :title="modalCategoria.accion + 'categoría'">
+      <form>
+        <div class="form-group">
+          <label for="nombre">Nombre</label>
+          <input v-model="modalCategoria.categoria.nombre" type="text" class="form-control" id="nombre" placeholder="Nombre de la categoría">
+        </div>
+        <div class = "form-group">
+          <label for="imagen">Imagen</label>
+          <!--Imagen anterior-->
+          <div v-if="modalCategoria.accion === 'Editar' && modalCategoria.categoria.imagen && !modalCategoria.categoria.nuevaImagen">
+            <img :src="modalCategoria.categoria.imagen" alt="Imagen de la categoría" class="img-thumbnail" width="200">
+          </div>
+          <!--Imagen nueva-->
+          <div v-else-if ="modalCategoria.categoria.nuevaImagen">
+            <img :src="modalCategoria.categoria.nuevaImagen" alt="Imagen de la categoría" class="img-thumbnail" width="200">
+          </div>
+            <input type="file" class="form-control-file" @change="cambioImagenCategoria" accept="image/*">
+        </div>
+      </form>
+      <template #footer>
+        <BotonComp @click="guardarCategoria">Guardar</BotonComp>
+        <BotonComp @click="modalCategoria.visible = false">Cancelar</BotonComp>
+      </template>
+    </b-modal>
     <div class = "footer">
       <p>
         Esto de aquí sería el footer, pero no se nos proporcionó la informacion necesaria
@@ -42,11 +68,23 @@
   <script>
   import ApiService from '@/services/ApiService'; 
 import BarraMenu from '@/components/BarraMenu.vue';
-import CategoriaComp from "@/components/CategoriaComp.vue";
+import BotonComp from '@/components/BotonComp.vue';
+import CategoriaComp from '@/components/CategoriaComp.vue';
   
   export default {
     data() {
       return {
+        modalCategoria: {
+          visible: false,
+          accion: 'Editar',
+          categoria: {
+            id: 6,
+            nombre: 'burgers',
+            imagen: 'https://th.bing.com/th/id/R.c691ed37c9ce3040c3ebd2892e88870c?rik=QUBcFflN%2b9oqPQ&pid=ImgRaw&r=0',
+            nuevaImagen: null,
+            archivoNuevaImagen: null
+          }
+        },
         categorias: [{
             id: 1,
             nombre: 'burgers',
@@ -77,6 +115,7 @@ import CategoriaComp from "@/components/CategoriaComp.vue";
     },
     components: {
       BarraMenu,
+      BotonComp,
       CategoriaComp,
     },
     methods: {
@@ -91,6 +130,31 @@ import CategoriaComp from "@/components/CategoriaComp.vue";
       },
         abrirMenu(categ) {
           window.location.href = '/menu?categoria=' + categ;
+        },
+        cambioImagenCategoria(event){
+          const archivo = event.target.files[0];
+          this.modalCategoria.categoria.archivoNuevaImagen = archivo;
+
+          if(archivo){
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              this.modalCategoria.categoria.nuevaImagen = e.target.result;
+            };
+            reader.readAsDataURL(archivo);
+          }
+          else{
+            this.modalCategoria.categoria.nuevaImagen = null;
+          }
+        },
+        guardarCategoria(){
+          if(this.modalCategoria.accion === 'Agregar'){
+            this.categorias.push(this.modalCategoria.categoria);
+          }
+          else{
+            const index = this.categorias.findIndex(categoria => categoria.id === this.modalCategoria.categoria.id);
+            this.categorias[index] = this.modalCategoria.categoria;
+          }
+          this.modalCategoria.visible = false;
         }
     },
     mounted() {
