f54ef533818cc35da82ed797cce7a828387d7e11
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-11-07 15:47:44 -0600

    Endpoint get funcional (faltan los detalles de ingredientes)

--- backend/menu/serializers.py
+++ backend/menu/serializers.py
@@ -14,3 +14,27 @@ class ProductoListaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producto
         fields = ['id', 'nombre', 'precio', 'valor_descuento', 'tipo_descuento', 'url_foto']
+
+class ProductoDetalleSerializer(serializers.ModelSerializer):
+    precio = serializers.SerializerMethodField()
+    precio_anterior = serializers.SerializerMethodField()
+    categoria = serializers.SerializerMethodField()
+    imagen = serializers.CharField(source='url_foto')
+    class Meta:
+        model = Producto
+        fields = ['id', 'nombre','descripcion', 'categoria', 'precio', 'precio_anterior','imagen']
+
+    def get_precio(self, obj):
+        if obj.descuento:
+            if obj.descuento.tipo == 'porcentaje':
+                return obj.precio - (obj.precio * obj.descuento.valor / 100)
+            return obj.precio - obj.descuento.valor
+        return obj.precio
+    
+    def get_precio_anterior(self, obj):
+        if obj.descuento:
+            return obj.precio
+        return None
+    
+    def get_categoria(self, obj):
+        return obj.categoria.nombre
