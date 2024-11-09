5124d4d64bbcf2810972e6f221675f83a6de20b8
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-11-08 20:14:34 -0600

    archivo creado y ruta configurada

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Login from "../views/LogIn.vue";
 import Principal from "../views/PaginaPrincipal.vue";
 import RegistroUsuario from "../views/RegistroUsuario.vue";
 import PaginaMenu from "../views/PaginaMenu.vue";
+import PaginaCuenta from "@/views/PaginaCuenta.vue";
 
 const routes = [
     {
@@ -26,6 +27,11 @@ const routes = [
         name: "Menu",
         component: PaginaMenu,
     },
+    {
+        path: "cuenta",
+        name: "Cuenta",
+        component: PaginaCuenta,
+    }
 ];
 
 const router = createRouter({
---
+++ frontend/src/views/PaginaCuenta.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
+
+<script>
+export default {
+    name: 'PaginaCuenta',
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
