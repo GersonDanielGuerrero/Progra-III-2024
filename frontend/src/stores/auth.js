00d1dae73d168f15646dbf0484f6ac144bc1aa23
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-11-10 07:55:38 -0600

    Guardado de usuario eliminado (de momento)

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -4,31 +4,25 @@ export const useAuthStore =
 defineStore('auth',{
     state: () => ({
         token: null,
-        usuario: null,
 }),
 actions: {
     setToken(token){
         this.token = token;
         localStorage.setItem('token', token);
         },
-    setUsuario(usuario){
-        this.usuario = usuario;
-        localStorage.setItem('usuario', JSON.stringify(usuario));
+        getToken(){
+            this.cargarDatos();
+            return this.token;
         },
     logout(){
         this.token = null;
         this.usuario = null;
         localStorage.removeItem('token');
-        localStorage.removeItem('usuario');
         },
         cargarDatos(){
             const token = localStorage.getItem('token');
             if(token){
                 this.token = token;
-            }
-            const usuario = localStorage.getItem('usuario');
-            if(usuario){
-                this.usuario = JSON.parse(usuario);
             }
         }
     }
