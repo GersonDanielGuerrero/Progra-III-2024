f9d9a5ef4db8f4d315754902df03e62024c13316
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-11-14 16:09:25 -0600

    Datos de ejemplo añadidos

--- frontend/src/views/ListaPedidos.vue
+++ frontend/src/views/ListaPedidos.vue
@@ -17,7 +17,46 @@ export default {
     name: 'ListaPedidos',
     data() {
         return {
-
+            pedidos:[
+                {
+                    id: 1,
+                    fecha: '2024-11-11 11:11:11',
+                    nombre_cliente: 'Juan Perez',
+                    tipo: 'Domicilio',
+                    direccion: '8va Calle poniente, Barrio La Parroquia, Santa Elena',
+                    cantidad_productos: 2,
+                    total: 12.50,
+                },
+                {
+                    id: 2,
+                    fecha: '2024-11-11 12:44:11',
+                    nombre_cliente: 'Maria Lopez',
+                    tipo: 'Local',
+                    direccion: '4ta Avenida Sur, Barrio El Calvario, Santa Elena',
+                    cantidad_productos: 1,
+                    total: 5.50,
+                },
+            ],
+            pedidosFiltrados: [
+                {
+                    id: 1,
+                    fecha: '2024-11-11 11:11:11',
+                    nombre_cliente: 'Juan Perez',
+                    tipo: 'Domicilio',
+                    direccion: '8va Calle poniente, Barrio La Parroquia, Santa Elena',
+                    cantidad_productos: 2,
+                    total: 12.50,
+                },
+                {
+                    id: 2,
+                    fecha: '2024-11-11 12:44:11',
+                    nombre_cliente: 'Maria Lopez',
+                    tipo: 'Local',
+                    direccion: '4ta Avenida Sur, Barrio El Calvario, Santa Elena',
+                    cantidad_productos: 1,
+                    total: 5.50,
+                },
+            ],
         };
     },
 }
