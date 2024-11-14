0cb6ce0283e096cfe160ba82005db863bc4af4b1
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-11-14 09:14:11 -0600

    Componente creado con código inicial

---
+++ frontend/src/components/BusquedaComp.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+    <CajaTexto type="text" v-model="busqueda" placeholder="Buscar..." @input ="buscar"/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+<script>
+import CajaTexto from './CajaTexto.vue';
+export default {
+    name: 'BusquedaComp',
+    components: {
+        CajaTexto,
+    },
+    data() {
+        return {
+            busqueda:"",
+        };
+    },
+    props: {
+        
+    },
+    methods: {
+        buscar(){
+            
+        }
+    },
+}
+</script>
