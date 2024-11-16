e2c4c642019c5f99c7424b2ae4a705fcb2a81668
Mayre <Mayrenapine30@outlook.es>
2024-11-16 14:40:05 -0600

    PreguntasFrecuentes

--- frontend/src/views/PreguntasFrecuentes.vue
+++ frontend/src/views/PreguntasFrecuentes.vue
@@ -1,23 +1,133 @@
 <template>
-    <div>
-        
+    <BarraMenu/>
+    <div class="customer-service">
+    <div class="header">
+      <h2>Atención al Cliente</h2>
+      <p>Bienvenidos. Atención al cliente. <span class="highlight">¿Qué le podemos ayudar?</span></p>
+      <div class="preguntas">
+      <p>Recomendaciones con preguntas y hacen seguido los clientes.</p>
+      </div>
     </div>
-</template>
-
+    <div class="questions">
+      <div class="question-item" v-for="(question, index) in questions" :key="index">
+        <span>{{ question }}</span>
+        <span class="actions">
+          <span class="icon">▼</span>
+          <span class="edit">✏</span>
+        </span>
+      </div>
+    </div>
+    <div class="AÑADIR">
+    <Botoncomp> AÑADIR PREGUNTA </Botoncomp>
+    </div>
+  </div>
+  </template>
+  
+ 
 <style scoped>
+customer-service {
+  text-align: center;
+  width: 400px;
+  margin: auto;
+  background-color: #000;
+  color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  min-height: 100vh;
+}
+
+.header h2 {
+  margin-bottom: 5px;
+  color: #ffad00;
+}
+.header 
+{
+  margin-bottom: 20px;
+  color: #ffad00;
+}
+.preguntas {
+  margin-bottom: 20px;
+  color: #ffffff;
+}
+
+.highlight {
+  color: #ffad00;
+}
+
+.questions {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 30px;
+  margin: 20px 180px;
+}
+
+.question-item {
+  background-color: #000;
+  border: 2px solid #ffad00;
+  color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.actions {
+  display: flex;
+  gap: 5px;
+}
+
+.icon, .edit {
+  color: #ffad00;
+  cursor: pointer;
+}
+
+.AÑADIR {
+    background-color: #ffad00;
+  color: #000;
+  padding: 0 16px; 
+  font-size: 0.9em; 
+  border-radius: 5px;
+  width: auto; 
+  display: inline-block; 
+  margin: 0 auto; 
+  text-align: center;
+  background-color: #000000;
+  color: #ffffff;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+
+}
 
 </style>
 
 <script>
-//Importaciones de componentes
-
-//Fin de importaciones de componentes
+import BarraMenu from '@/components/BarraMenu.vue';
+import Botoncomp from '@/components/BotonComp.vue';
 export default {
     name: 'PreguntasFrecuentes',
+    components: {
+     BarraMenu,
+     Botoncomp
+
+    },
     data() {
         return{
-
-        };
-    }
+            questions: [
+        "¿A qué hora abre el local?",
+        "¿Hasta dónde llegan los envíos?",
+        "Pregunta de ejemplo",
+        "Pregunta de ejemplo",
+        "Pregunta de ejemplo",
+        "Pregunta de ejemplo"
+      ]
+    };
+  },
+  methods: {
+    addQuestion() {
+      alert("Nueva pregunta agregada");
+        }
+    },
 }
 </script>
