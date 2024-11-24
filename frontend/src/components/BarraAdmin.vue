fc4060fff122f56fe611109960843a843f0d5357
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-11-24 13:50:34 -0600

    Componente creado

---
+++ frontend/src/components/BarraAdmin.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+    name: 'BarraAdmin',
+    data() {
+        return{
+
+        }
+    },
+    methods: {
+        
+    },
+}
+
+</script>
