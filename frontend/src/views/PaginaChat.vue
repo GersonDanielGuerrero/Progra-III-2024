40144c022b9f0261358b35e6d0215cda38ca473a
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-11-24 14:17:50 -0600

    Error en input de mensaje corregido

--- frontend/src/views/PaginaChat.vue
+++ frontend/src/views/PaginaChat.vue
@@ -51,7 +51,7 @@
         <CajaTexto 
           v-model="mensajeActual" 
           placeholder="Escribe tu mensaje..."
-          @keydown.enter="enviarMensaje"
+          enter="enviarMensaje"
         />
         <button 
             class="btn btn-primary enviar-btn" 
