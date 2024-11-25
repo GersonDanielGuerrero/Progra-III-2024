d5fd3c34c222ebc8dc9c964312d46fc8aeb35b9f
GersonDanielGuerrero <danicastillog4@gmail.com>
2024-11-25 15:36:01 -0600

    GET mensajes

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,21 @@
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import ChatSerializer
+from rest_framework.response import Response
+from .models import Mensaje
+from usuarios.models import Usuario
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class ChatView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        usuario = request.user
+        id_cliente = request.query_params.get('id_cliente')
+        version_ia = request.query_params.get('version_ia')
+        tipo_usuario = 'cliente' if usuario.roles.filter(nombre='Cliente').exists() else 'empleado' if usuario.roles.filter(nombre='Atención al cliente').exists() else None
+        if not tipo_usuario:
+            return Response(status=status.HTTP_403_FORBIDDEN, data={'mensaje': 'No tienes permisos para realizar esta acción'})
+        serializer = ChatSerializer(instance=usuario, context={'id_cliente': id_cliente, 'version_ia': version_ia, 'tipo_usuario': tipo_usuario})
+        return Response(serializer.data)
